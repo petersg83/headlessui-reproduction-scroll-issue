abed2280f8ba4de3f66369df598457cae3a50c09
Pierre NOËL <contact@pierre-noel.fr>
2024-10-04 14:18:14 +0200

    add bug reproduction code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { useState } from 'react'
+
+const people = Array(60).fill().map((_, i) => ({
+  id: i,
+  name: `name-${i}`
+}))
 
 function App() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0])
+  const isInIframe = window.self !== window.top;
+  console.log(isInIframe)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+        <ListboxButton>Select that  {isInIframe ? 'cannot' : 'can'} scroll</ListboxButton>
+        <ListboxOptions anchor="bottom">
+          {people.map((person) => (
+            <ListboxOption key={person.id} value={person} className="data-[focus]:bg-blue-100">
+              {person.name}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+
+      {!isInIframe && <iframe title="title" src="http://localhost:3000" width="100%" marginheight="0" frameborder="0" height="1500px"></iframe>}
     </div>
-  );
+  
+  )
 }
 
 export default App;
